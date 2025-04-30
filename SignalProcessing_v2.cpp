3393f36209cf797b514171d5d22ebca343549dca
temaxuck <gorropand@gmail.com>
2025-04-30 16:18:41 +0300

    A pinch of refactoring

--- SignalProcessing_v2.cpp
+++ SignalProcessing_v2.cpp
@@ -1,115 +1,124 @@
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <map>
-#include <tuple>
 #include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <malloc.h>
+#include <map>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
 
 using namespace std;
+namespace fs = std::filesystem; // Just an alias
 
-const int run_start = 1;
-const int run_stop = 1;
+const size_t DATA_OBJECT_SIZE = 2; // number of bytes per data object
+typedef int16_t data_t; // sizeof(DATA_OBJECT_TYPE) == DATA_OBJECT_SIZE
 
+const int RUN_START = 1;
+const int RUN_STOP = 1;
 
-string file_name = "C:\\code_root\\data_in\\f1";
-const int events_per_run = 1000;
-const int points_per_event = 9999;
+const fs::path DATA_DIR = "./data/";
+const int EVENTS_PER_RUN = 1000;
+const int POINTS_PER_EVENT = 9999;
 
 typedef enum {
-	CHANNEL_SLOW = 2, 
-	CHANNEL_FAST = 5,
-}CHANNEL;
+    CHANNEL_SLOW,
+    CHANNEL_FAST,
+} CHANNEL_TYPE;
 
 typedef struct {
-	int x;
-	int y;
-}Point;
+    CHANNEL_TYPE t;
+    int n;
+} Channel;
 
-struct Events{
-	int event;
-	int run;
+typedef struct {
+    int x;
+    int y;
+} Point;
 
-	bool operator<(const Events& other) const {
+typedef vector<Point> Event;
+
+struct Events {
+    int event;
+    int run;
+
+    bool operator<(const Events& other) const {
         return tie(run, event) < tie(other.run, other.event);
     }
 
 };
 
-vector<int> Read_by_runs(CHANNEL ch, int run){
-	vector <int> signal_run;
-	signal_run.resize(events_per_run*points_per_event);
+vector<data_t> read_data(fs::path filepath, size_t n_obj) {
+    vector<data_t> data(n_obj);
 
-	FILE *f = NULL;
-	
-	ostringstream _run, _ch;
-	_run << run;
-	_ch  << ch;
-	string channel =  "__ch_" + _ch.str() + ".dat";
-	string filepath = file_name + "\\run_" + _run.str() + channel;
+    FILE* f = fopen(filepath.c_str(), "rb");
+    if (!f) {
+        cout << "Error opening file: " << filepath << endl;
+        exit(EXIT_FAILURE);
+    }
+    fread(data.data(), DATA_OBJECT_SIZE, n_obj, f);
+    fclose(f);
 
-	f = fopen(filepath.c_str(), "rb");
-
-	if (!f) {
-		cout << "Error opening file: " << filepath << endl;
-		exit(EXIT_FAILURE);
-	}
-
-	fread(signal_run.data(), 2, events_per_run*points_per_event, f);
-	fclose(f); 
-
-	return signal_run;
-} 
-
-vector<Point> Turn_origin_to_point(CHANNEL ch, vector<int> signal){
-	int coeff;
-	if (ch == CHANNEL_SLOW) coeff = 1;
-	if (ch == CHANNEL_FAST) coeff = -1;
-	
-	vector<Point> signal_points;
-	signal_points.reserve(signal.size()); 
-	Point point;
-	for (int points = 0; points < signal.size(); points++){
-		point.x = points;
-		point.y = coeff*signal[points];
-		signal_points.push_back(point);
-	} 
-	return signal_points;
-}
-
-vector<vector<Point>> Parsing_data_on_events(vector<Point> signal){
-	vector<vector<Point>> signal_events;
-	for (int event = 0; event < events_per_run; event++)
-	{
-		vector <Point> temp(signal.begin() + event*points_per_event, signal.begin() + (event+1)*points_per_event);
-		signal_events.push_back(std::move(temp));
-	}
-
-	return signal_events;
-}
-
-map<Events, vector<Point>> Mapping_data_with_runs_events(int run, vector<vector<Point>> signal){
-	map<Events, vector<Point>> mapped_signal;
-	for(int event = 0; event < signal.size(); event++){
-		Events run_event;
-		run_event.run = run;
-		run_event.event = event;
-		mapped_signal.insert({run_event, signal[event]}); 
-	}
-	return mapped_signal;
+    return data;
 }
 
 
+// Read data from channel `ch` and run `run`
+vector<data_t> read_data_by_run(Channel chan, int run) {
+    char filename[32];
+    sprintf(filename, "run_%d__ch_%d.dat", run, chan.n);
+    fs::path filepath = DATA_DIR / filename;
 
-int Signal_processing2()
-{	
-	
-	vector<vector<Point>> signal;
-	signal = Parsing_data_on_events(Turn_origin_to_point(CHANNEL_SLOW, Read_by_runs(CHANNEL_SLOW, run_start)));
-	auto ssignal = Mapping_data_with_runs_events(run_start, signal);
-	for (const auto& pair : ssignal){
-		cout << pair.first.run << " " << pair.first.event << " " << pair.second[9999].y << endl; 
-	}
-	return 0;
+    return read_data(filepath, POINTS_PER_EVENT * EVENTS_PER_RUN);
+}
+
+vector<Event> split_data_to_events(Channel chan, vector<data_t> data) {
+    int coeff;
+    if (chan.t == CHANNEL_SLOW) coeff = 1;
+    else if (chan.t == CHANNEL_FAST) coeff = -1;
+
+    vector<Event> events(EVENTS_PER_RUN);
+    for (size_t i = 0; i < EVENTS_PER_RUN; i++) {
+      Event event(POINTS_PER_EVENT);
+      for (size_t j = 0; j < POINTS_PER_EVENT; j++) {
+        event[j] = Point {
+          .x = (int)j,
+          .y = coeff*data[i * POINTS_PER_EVENT + j],
+        };
+      }
+      events[i] = event;
+    }
+    
+    return events;
+}
+
+// TODO: This must not be done like this. It would take a gazillion
+// operations to compare 2 keys of the map. Discuss the meaning of
+// that later and come up with a better solution.
+map<Events, Event> mapping_data_with_runs_events(int run, vector<Event> signal) {
+    map<Events, Event> mapped_signal;
+    for(size_t i = 0; i < signal.size(); i++) {
+        Events run_event;
+        run_event.run = run;
+        run_event.event = i;
+        mapped_signal.insert({run_event, signal[i]});
+    }
+    return mapped_signal;
+}
+
+int SignalProcessing_v2() {
+    Channel chan = {
+        .t = CHANNEL_SLOW,
+        .n = 2,
+    };
+    vector<data_t> data = read_data_by_run(chan, RUN_START);
+    vector<Event> events = split_data_to_events(chan, data);
+
+    // TODO: Discuss this thingy
+    auto ssignal = mapping_data_with_runs_events(RUN_START, events);
+    for (const auto& pair : ssignal) {
+        cout << pair.first.run << " " << pair.first.event << " " << pair.second[9999].y << endl;
+    }
+    return 0;
 }
